9e821ec2fab6e41a17423e2045008c5a6a5a2d33
Viwu <sinconsistencia@gmail.com>
2025-11-21 06:51:48 -0600

    mapTiles sprite group todo

--- source/sphis/trijam347/PlayState.hx
+++ source/sphis/trijam347/PlayState.hx
@@ -1,12 +1,18 @@
 package sphis.trijam347;
 
 import flixel.FlxState;
+import flixel.group.FlxSpriteGroup;
 
 class PlayState extends FlxState
 {
+	var mapTiles:FlxSpriteGroup;
+
 	override public function create()
 	{
 		super.create();
+		
+		mapTiles = new FlxSpriteGroup();
+		add(mapTiles);
 	}
 
 	override public function update(elapsed:Float)
