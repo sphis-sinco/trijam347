d4fa46d548a70703bbb9e5709d02faab6ea38c77
Viwu <sinconsistencia@gmail.com>
2025-11-21 15:45:52 -0600

    gameover screen will work in a slide format

--- source/sphis/trijam347/GameOver.hx
+++ source/sphis/trijam347/GameOver.hx
@@ -1,8 +1,32 @@
 package sphis.trijam347;
 
+import flixel.FlxG;
 import flixel.FlxState;
 
 class GameOver extends FlxState
 {
-    
+	public var slide:Int = 1;
+
+	override function create()
+	{
+		super.create();
+
+		slide = 1;
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (FlxG.keys.justReleased.SPACE)
+		{
+			slide++;
+			changeSlide();
+		}
+	}
+
+	public function changeSlide()
+	{
+		switch (slide) {}
+	}
 }
