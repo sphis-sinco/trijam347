ae814552f17fcef41c5491ae83bd13fdf4feba64
Viwu <sinconsistencia@gmail.com>
2025-11-21 16:29:35 -0600

    footsteps

--- source/sphis/trijam347/GameOver.hx
+++ source/sphis/trijam347/GameOver.hx
@@ -11,23 +11,23 @@ class GameOver extends FlxState
 {
 	public var slide:Int = 1;
 
-    var chebys:FlxSprite;
-    var player:Player;
+	var chebys:FlxSprite;
+	var player:Player;
 
 	override function create()
 	{
 		super.create();
 
 		slide = 1;
-        
-        player = new Player();
+
+		player = new Player();
 		player.scale.set(4, 4);
 		player.updateHitbox();
-        player.screenCenter();
-        player.x -= player.width * 2;
-        add(player);
+		player.screenCenter();
+		player.x -= player.width * 2;
+		add(player);
 
-        changeSlide();
+		changeSlide();
 	}
 
 	override function update(elapsed:Float)
@@ -43,32 +43,43 @@ class GameOver extends FlxState
 
 	public function changeSlide()
 	{
-		switch (slide) {
-            case 1:
-                chebys = new FlxSprite();
-                chebys.loadGraphic('assets/images/gameoverslide/chebys.png');
-                
-                chebys.scale.set(4,4);
-                chebys.updateHitbox();
-                chebys.screenCenter();
+		switch (slide)
+		{
+			case 1:
+				chebys = new FlxSprite();
+				chebys.loadGraphic('assets/images/gameoverslide/chebys.png');
 
-                chebys.scale.set(8,8);
-                chebys.updateHitbox();
-                chebys.screenCenter(X);
-                chebys.x += chebys.width / 4;
-                
-                add(chebys);
-                FlxG.camera.flash(FlxColor.BLACK);
-            case 2:
-                FlxG.sound.play('assets/sounds/open-door.wav', 1.0);
-                chebys.loadGraphic('assets/images/gameoverslide/chebys-open.png');
-                FlxTween.tween(chebys, {y: FlxG.height * 2}, 4, {
-                    onComplete: t -> {
-                        chebys.visible = false;
-                    },
-                    ease: FlxEase.sineInOut,
-                    startDelay: 1
-                });
-        }
+				chebys.scale.set(4, 4);
+				chebys.updateHitbox();
+				chebys.screenCenter();
+
+				chebys.scale.set(8, 8);
+				chebys.updateHitbox();
+				chebys.screenCenter(X);
+				chebys.x += chebys.width / 4;
+
+				add(chebys);
+				FlxG.camera.flash(FlxColor.BLACK);
+			case 2:
+				FlxG.sound.play('assets/sounds/open-door.wav', 1.0);
+				chebys.loadGraphic('assets/images/gameoverslide/chebys-open.png');
+				FlxTween.tween(chebys, {y: FlxG.height * 2}, 4, {
+					onComplete: t ->
+					{
+						chebys.visible = false;
+					},
+					ease: FlxEase.sineInOut,
+					startDelay: 1,
+					onStart: t ->
+					{
+						player.animation.play('walk');
+						FlxG.sound.play('assets/sounds/footsteps.wav', 1.0, false, null, true, () ->
+						{
+							player.animation.pause();
+							player.animation.frameIndex = 0;
+						});
+					}
+				});
+		}
 	}
 }
